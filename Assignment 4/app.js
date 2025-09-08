efa8621ba0834abbf923b561b908ca6893f569e6
Laibarao2004 <laibarao393@gmail.com>
2025-09-08 11:59:08 -0700

    Add files via upload

---
+++ Assignment 4/app.js
@@ -0,0 +1,24 @@
+// Assignment 4  //
+// Legal & Illegal  //
+// 1. Declare 3 variables in one statement //
+
+// let name ,age , city;
+
+// 2. Declare 5 legal & 5 illegal variable names. //
+
+// legal  //
+let myName, $price, _value, user1, totalamount;
+
+// illegal  //
+// let 23Name;
+// let my-name;
+// let var;
+// let user name;
+// let alert;
+
+// 3. Display this in your browser //
+document.write( "<h1>Rules for naming JS variables</h1> <br/>");
+document.write("Variable names can only contain letters, number , $ and _. For example: $my_1stvariable<br/>");
+document.write("Variables must begin with a letter , $ or _. For example: $name,  _name or name <br/>");
+document.write("Variable names are case sensitive<br/>");
+document.write("Variable names should not be JS keywords");
